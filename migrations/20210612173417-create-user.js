6cc8d2d1be68024759bd4e84030f6cfa00fa154d
Fahd_ZOUHAIR <fahdzouhair47@gmail.com>
2021-06-12 19:40:32 +0200

    quest 9 & 10

--- migrations/20210612173417-create-user.js
+++ migrations/20210612173417-create-user.js
@@ -9,10 +9,13 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       username: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false
       },
       email: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false
       },
       password: {
         type: Sequelize.STRING
