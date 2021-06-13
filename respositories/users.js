35fcb41c7ff51a8f9b4b9e09afd8b36e3facb1e9
Fahd_ZOUHAIR <fahdzouhair47@gmail.com>
2021-06-13 22:21:23 +0200

    quest 13 & 14

---
+++ respositories/users.js
@@ -0,0 +1,66 @@
+const { User } = require('../models');
+
+
+
+module.exports = {
+
+    getAllUsers() {
+        return User.findAll();
+    },
+    
+    getUsers(offset=0, limit=10) {
+        return User.findAll({
+            offset: offset,
+            limit: limit
+        });
+    },
+
+    getAdmins() {
+        return User.findAll({
+            where: {
+                role: "admin"
+            }
+        });
+    },
+
+    getAuthors() {
+        return User.findAll({
+            where: {
+                role: "author"
+            }
+        });
+    },
+
+    getGuests() {
+        return User.findAll({where: {role: "guest"}});
+    },
+
+    getUser(id) {
+        return User.findAll({where:{id: id}});
+    },
+
+    getUserByEmail(email) {
+        return User.findAll({where: {email: email}});
+    },
+
+    addUser(user) {
+        User.create(user);
+    },
+
+    updateUser(id,user) {
+        User.update(user, {
+            where: {
+                id: id
+            }
+        });
+    },
+
+    deleteUser(id) {
+        User.destroy({  
+            where: {
+                id: id,
+            }
+        })
+    },
+
+}
--- seeders/20210612174501-users-create.js
+++ seeders/20210612174501-users-create.js
@@ -1,7 +1,33 @@
 'use strict';
+const faker = require('faker');
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+
+    const roles1 = ['admin','author','guest'];
+    let User = [];
+
+    for (let i=0 ; i<21 ; i++){
+        let username;
+        let email;
+        let password; 
+        let role;
+        let createdAt;
+        let updatedAt;
+        User.push({
+            id  : i,
+            username:faker.internet.userName(),
+            email:faker.internet.email(),
+            password:faker.internet.password(),
+            role:roles1[parseInt(Math.random()*3)],
+            createdAt:faker.date.recent(),
+            updatedAt:faker.date.future()
+        })
+    }
+
+    var x1 = queryInterface.bulkInsert('users',User,{}) ;
+    return Promise.all([x1]);
+
     /**
      * Add seed commands here.
      *
